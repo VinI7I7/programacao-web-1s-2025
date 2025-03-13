c6c035c2cacb6bac4881df607129194ece993118
VinI7I7 <contato.ll.ribeiro@gmail.com>
2025-03-13 09:12:47 -0300

    agora foi

--- atividade2/verificarNumeroPrimo.js
+++ atividade2/verificarNumeroPrimo.js
@@ -14,3 +14,13 @@ function verificarNumeroPrimo(n){
     return true;
 }
 
+console.log('verificarNumeroPrimo(0);='+verificarNumeroPrimo(0)); //false
+console.log('verificarNumeroPrimo(1);='+verificarNumeroPrimo(1)); //false
+console.log('verificarNumeroPrimo(2);='+verificarNumeroPrimo(2)); //true
+console.log('verificarNumeroPrimo(3);='+verificarNumeroPrimo(3)); //true
+console.log('verificarNumeroPrimo(7);='+verificarNumeroPrimo(7)); //true
+console.log('verificarNumeroPrimo(83);='+verificarNumeroPrimo(83)); //true
+console.log('verificarNumeroPrimo(100);='+verificarNumeroPrimo(100)); //false
+console.log('verificarNumeroPrimo(991);='+verificarNumeroPrimo(991)); //true
+console.log('verificarNumeroPrimo(104729);='+verificarNumeroPrimo(104729)); //true
+console.log('verificarNumeroPrimo(14348907);='+verificarNumeroPrimo(14348907)); //false
